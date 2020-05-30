c224ff8fd3e5c3a2759924e994cca077d686bc67
@trung_vu <trungvu0011@gmail.com>
2020-05-30 10:58:57 +0700

    frond end ReactJS final project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navbar from './components/navbar';
+import Body from './components/body';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <Body/> 
   </React.StrictMode>,
   document.getElementById('root')
 );
