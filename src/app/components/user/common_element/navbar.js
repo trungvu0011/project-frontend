3aca578338694ce35594ce81a51b5004c2d5f506
@trung_vu <trungvu0011@gmail.com>
2020-06-08 17:25:18 +0700

    commit auth token

--- src/app/components/user/common_element/navbar.js
+++ src/app/components/user/common_element/navbar.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Popup from "reactjs-popup";
+import axios from 'axios'
 
 import Home from '../../../components/user/home'
 import DoctorBooking from '../../../components/user/doctor_booking';
@@ -30,10 +31,203 @@ import {
 } from "react-router-dom";
 
 function Navbar() {
+  let name='';
+  let jwt=''
+
+  let nameDoctor='';
+  let jwtDoctor=''
+
+  useEffect(() => {
+    const banlabacsi = document.getElementById("banlabacsi");
+      const dangky = document.getElementById("dangky");
+      const dathenkham = document.getElementById("dathenkham");
+
+   console.log("after f5",window.sessionStorage.accessToken)
+    console.log(window.sessionStorage.getItem('loginType'));
+    if(window.sessionStorage.accessToken)
+    {
+      if (name!==null) {
+        banlabacsi.style.display = "none";
+        dangky.style.display = "none";
+        dathenkham.style.display = "none";
+      }
+      axios.post('https://final-wcy-backend.herokuapp.com/authentication/'+window.sessionStorage.getItem('loginType'), {
+        accessToken: window.sessionStorage.accessToken,
+        strategy:"jwt"
+      })
+      .then(function (response) {
+        // console.log(response);
+        // console.log(response.data.accessToken);
+        // console.log(response.data.user.name);
+        if(window.sessionStorage.getItem('loginType')==='users'){
+          name=response.data.user.name;
+        }
+        else{
+          name=response.data.doctor.name;
+        }
+        
+        // jwt=response.data.accessToken;
+        // window.sessionStorage.accessToken = response.data.accessToken;
+        // window.sessionStorage.setItem('loginType', 'users');
+        // console.log("Window token")
+        // console.log(window.sessionStorage.accessToken)
+      })
+      .catch(function (error) {
+        console.log(error);
+        window.sessionStorage.accessToken=null;
+      })
+      .then(function () {
+        // always executed
+        if (name!==null) {
+          banlabacsi.style.display = "none";
+          dangky.style.display = "none";
+          dathenkham.style.display = "none";
+        }
+      });  
+    }
+    
+  }, null)
+
+  const handleSignUp = () =>{
+    
+    const name = document.getElementById("Form-signup-name").value;
+    const email= document.getElementById("Form-signup-email").value;
+    const pass1= document.getElementById("Form-pass1").value;
+    const pass2= document.getElementById("Form-pass2").value;
+
+    if(pass1 !== pass2){
+      alert("Nhap lai mat khau")
+      return
+    }
+
+    axios.post('https://final-wcy-backend.herokuapp.com/users', {
+      name: name,
+      email: email,
+      password: pass1,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    }
+
+    const handleSignIn = () =>{
+      const email= document.getElementById("Form-signin-email").value;
+      const pass= document.getElementById("Form-signin-pass").value;
+
+      const banlabacsi = document.getElementById("banlabacsi");
+      const dangky = document.getElementById("dangky");
+      const dathenkham = document.getElementById("dathenkham");
+
+      if(email ===null || pass===null)
+      {
+        alert("Nhap email & password")
+        return
+      }
+      axios.post('https://final-wcy-backend.herokuapp.com/authentication/users', {
+        email: email,
+        password: pass,
+        strategy: "local",
+      })
+      .then(function (response) {
+        console.log(response);
+        console.log(response.data.accessToken);
+        console.log(response.data.user.name);
+        name=response.data.user.name;
+        jwt=response.data.accessToken;
+        window.sessionStorage.accessToken = response.data.accessToken;
+        window.sessionStorage.setItem('loginType', 'users');
+        console.log("Window token")
+        console.log(window.sessionStorage.accessToken)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+        if (name!==null) {
+          banlabacsi.style.display = "none";
+          dangky.style.display = "none";
+          dathenkham.style.display = "none";
+        }
+      });     
+      }
+
+      const handleSignInFB = () =>{
+        axios.post('https://final-wcy-backend.herokuapp.com/authentication/users', {
+          strategy: "facebook",   
+        })
+        .then(function (response) {
+          console.log(response);
+          console.log(response.data.accessToken);
+          console.log(response.data.user.name);
+          name=response.data.user.name;
+          jwt=response.data.accessToken;
+
+          window.sessionStorage.accessToken = response.data.accessToken;
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });  
+        // const dangnhap = document.getElementById("dangnhap");
+        // const dangky = document.getElementById("dangky");
+        // const dathenkham = document.getElementById("dathenkham");
+  
+        // if (name!==null) {
+        //   dangnhap.style.display = "none";
+        //   dangky.style.display = "none";
+        //   dathenkham.style.display = "none";
+        // }
+        }
+
+      const handleDoctorSignIn=()=>{
+          const email= document.getElementById("Form-email-doctor").value;
+          const pass= document.getElementById("Form-pass-doctor").value;
+
+          const banlabacsi = document.getElementById("banlabacsi");
+          const dangky = document.getElementById("dangky");
+          const dathenkham = document.getElementById("dathenkham");
+  
+      if(email ===null || pass===null)
+      {
+        alert("Nhap email & password")
+        return
+      }
+      axios.post('https://final-wcy-backend.herokuapp.com/authentication/doctors', {
+        email: email,
+        password: pass,
+        strategy: "local",
+      })
+      .then(function (response) {
+        console.log(response);
+        console.log(response.data.accessToken);
+        console.log(response.data.doctor.name);
+        nameDoctor=response.data.doctor.name;
+        jwtDoctor=response.data.accessToken
+        window.sessionStorage.accessToken = response.data.accessToken;
+        window.sessionStorage.setItem('loginType', 'doctors');
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        if (nameDoctor!==null) {
+          banlabacsi.style.display = "none";
+          dangky.style.display = "none";
+          dathenkham.style.display = "none";
+        }
+      });  
+        }
+  
   return (
     <div>
        <Router>
-            
                 {/* <Redirect from="/" to="/maps"/> */}
       <section id="nav-bar">
         <nav id="collapsibleNav" className="navbar-expand-lg fixed-top">
@@ -71,7 +265,6 @@ function Navbar() {
                   <Link className="nav-link" title="Thông Tin Phòng Khám" to="/doctor-booking">
                     <span className="menu-icon">
                       <img src={medicine} alt="hinh anh" />
-                
                     </span>
                     <span>Thông Tin Phòng Khám</span>
                   </Link>
@@ -87,7 +280,7 @@ function Navbar() {
               </ul>
               <div className="btn-toolbar mb-1">
                 <div className="btn-group mr-2">
-                  <Popup modal trigger={<button className="btn btn-success">Đặt Hẹn Khám Ngay</button>}>
+                  <Popup modal trigger={<button className="btn btn-success" id="dathenkham" >Đặt Hẹn Khám Ngay</button>}>
                     <div>
                       <div className="modal-content form-elegant">
                         <div className="text-center">
@@ -99,20 +292,20 @@ function Navbar() {
                             
                              <div className="md-form mb-5">
                               <h4> Đăng nhập bằng tài khoản</h4>
-                              <input type="email" id="Form-email1" className="form-control validate" />
+                              <input type="email" id="Form-signin-email" className="form-control validate" />
                             </div>
 
                             <div className="md-form pb-3">
                               <h4> Mật khẩu</h4>
-                              <input type="password" id="Form-pass1" className="form-control validate" />
+                              <input type="password" id="Form-signin-pass" className="form-control validate" />
                             </div>
                             <div className="text-center mb-3">
-                              <button id="main-button" type="button" className="btn btn-block btn-rounded z-depth-1a">Đăng Nhập</button>
+                              <button id="dangnhap" type="button" className="btn btn-block btn-rounded z-depth-1a" onClick={handleSignIn}>Đăng Nhập</button>
                             </div>
                             <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2">
                               hoặc đăng nhập bằng:</p>
                             <div className="row my-3 d-flex justify-content-center">
-                              <button type="button" className="btn btn-white btn-rounded mr-md-3 z-depth-1a">
+                              <button onClick={handleSignInFB} type="button" className="btn btn-white btn-rounded mr-md-3 z-depth-1a">
                                 <i className="fab fa-facebook-f text-center" />
                               </button>
                               <button type="button" className="btn btn-white btn-rounded z-depth-1a">
@@ -133,46 +326,35 @@ function Navbar() {
             {/* Đăng nhập button */}
             <div className="btn-toolbar mb-1">
               <div className="btn-group mr-2">
-                <Popup modal trigger={<button className="btn btn-light btn-sm rounded-bottom"><b>Đăng Nhập</b></button>}>
-                  <div>
-                    <div className="modal-content form-elegant">
-                      <div className="text-center">
-                        <h3 className="modal-title w-100 dark-grey-text my-3" id="myModalLabel">
-                          <strong>Đăng Nhập</strong>
-                        </h3>
-                        <div className="modal-body mx-4">
-                          {/*Body*/}
-                          <div className="md-form mb-5">
-                            <h4> Đăng nhập bằng tài khoản</h4>
-                            <input type="email" id="Form-email1" className="form-control validate" />
-                          </div>
+              <Popup modal trigger={<button className="btn btn-light btn-sm rounded-bottom" id="banlabacsi"><b>Bạn là bác sĩ ?</b></button>}>
+              <div>
+              <div className="modal-content form-elegant">
+              <div className="text-center">
+              <h3 className="modal-title w-100 dark-grey-text font-weight-bold my-3" id="myModalLabel"><strong>Chào bác sĩ</strong></h3>
+              <div className="modal-body mx-4">
+              {/*Body*/}
+              <div className="md-form mb-5">
+                <h4> Đăng nhập</h4>
+                <input type="email" id="Form-email-doctor" className="form-control validate" />
+              </div>
 
-                          <div className="md-form pb-3">
-                            <h4> Mật khẩu</h4>
-                            <input type="password" id="Form-pass1" className="form-control validate" />
-                          </div>
-                          <div className="text-center mb-3">
-                            <button id="main-button" type="button" className="btn btn-block btn-rounded z-depth-1a">Đăng Nhập</button>
-                          </div>
-                          <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2">
-                            hoặc đăng nhập bằng:</p>
-                          <div className="row my-3 d-flex justify-content-center">
-                            <button type="button" className="btn btn-white btn-rounded mr-md-3 z-depth-1a">
-                              <i className="fab fa-facebook-f text-center" />
-                            </button>
-                            <button type="button" className="btn btn-white btn-rounded z-depth-1a">
-                              <i className="fab fa-google-plus-g" />
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </Popup>
+              <div className="md-form pb-3">
+              <h4> Mật khẩu</h4>
+                <input type="password" id="Form-pass-doctor" className="form-control validate" />
+              </div>
+              <div className="text-center mb-3">
+                <button onClick={handleDoctorSignIn} id="main-button" type="button" className="btn btn-block btn-rounded z-depth-1a">Đăng Nhập</button>
+              </div>
+             
+            </div>
+            </div>
+      </div>
+              </div>
+      </Popup>
               </div>
               
               <div className="btn-group">
-                <Popup modal trigger={<button className="btn btn-light btn-sm rounded-bottom" ><b>Đăng Ký</b></button>}>
+                <Popup modal trigger={<button className="btn btn-light btn-sm rounded-bottom" id="dangky"><b>Đăng Ký</b></button>}>
                   {/*Content*/}
                   <div className="modal-content form-elegant">
                     {/*Header*/}
@@ -183,26 +365,26 @@ function Navbar() {
                     <div className="modal-body mx-4">
                       {/*Body*/}
                       <div className="md-form mb-5">
-                        <input type="email" id="Form-email1" className="form-control validate" />
+                        <input type="text" id="Form-signup-name" className="form-control validate" />
                         <label data-error="wrong" data-success="right" htmlFor="Form-email1">Họ &amp; tên đầy đủ</label>
                       </div>
                       <div className="md-form pd-1">
-                        <input type="password" id="Form-pass1" className="form-control validate" />
-                        <label data-error="wrong" data-success="right" htmlFor="Form-pass1">Số điện thoại</label>
+                        <input type="email" id="Form-signup-email" className="form-control validate" />
+                        <label data-error="wrong" data-success="right" htmlFor="Form-pass1">Email</label>
                       </div>
                       <div className="md-form pd-1">
                         <input type="password" id="Form-pass1" className="form-control validate" />
                         <label data-error="wrong" data-success="right" htmlFor="Form-pass1">Tạo một mật khẩu</label>
                       </div>
                       <div className="md-form pd-1">
-                        <input type="password" id="Form-pass1" className="form-control validate" />
+                        <input type="password" id="Form-pass2" className="form-control validate" />
                         <label data-error="wrong" data-success="right" htmlFor="Form-pass1">Nhập lại mật khẩu</label>
                       </div>
                       <form>
                         <label><input type="checkbox" name="personality" defaultChecked /> Nhận thông tin &amp; khuyến mãi của chúng tôi qua email</label><br />
                       </form>
                       <div className="text-center mb-3">
-                        <button id="main-button" type="button" className="btn btn-block  btn-rounded z-depth-1a">Đăng Ký</button>
+                        <button id="main-button" type="button" className="btn btn-block  btn-rounded z-depth-1a" onClick={handleSignUp}>Đăng Ký</button>
                       </div>
                     </div>
                     {/*Footer*/}
